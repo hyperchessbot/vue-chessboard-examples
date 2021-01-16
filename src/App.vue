a9799419015edc8f3577a8e263abe196446efdf2
hyperchessbot <hyperchessbot@gmail.com>
2021-01-16 13:21:06 +0000

    Set fen from outside app

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,14 @@
 <template>
-  <div id="app">    
-    <chessboard/>
+  <div id="app" ref="board">    
+    <input style="display:none;" type="text" id="fen"/>
+    <button id="clickbutton" style="display:none;" @click="setFen()"/>
+    <chessboard :fen="currentFen"/>    
+    <div class="show-fen" v-for="fen in fens" :key="fen + '-binded'">    
+    Set FEN to 
+        <button class="button is-light show-fen" @click="setFen(fen)">
+        {{fen}}
+        </button>
+    </div>
   </div>
 </template>
 
@@ -9,22 +17,24 @@ import {chessboard} from 'vue-chessboard'
 import 'vue-chessboard/dist/vue-chessboard.css'
 
 export default {
-  name: 'app',
+  name: 'app',  
   components: {
-    chessboard,
+      chessboard,
   },
   data () {
-    return {
-      currentFen: '',
-      positionInfo: null
+    return {        
+        currentFen: '',
+        positionInfo: null,      
     }
   },
   methods: {
     showInfo(data) {
       this.positionInfo = data
-    },
-    loadFen(fen) {
-      this.currentFen = fen
+    },    
+    setFen(fen){
+        const inp = document.getElementById("fen")        
+        if(fen) inp.value = fen
+        this.currentFen = inp.value
     },
     promote() {
       if (confirm("Want to promote to rook? Queen by default") ) {
@@ -38,6 +48,6 @@ export default {
     this.fens = ['5rr1/3nqpk1/p3p2p/Pp1pP1pP/2pP1PN1/2P1Q3/2P3P1/R4RK1 b - f3 0 28',
                 'r4rk1/pp1b3p/6p1/8/3NpP2/1P4P1/P2K3P/R6R w - - 0 22'
                 ]
-  }
+  },
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,17 @@
 import Vue from 'vue'
 import App from './App.vue'
+import '../public/app.css'
+
 new Vue({
   el: '#app',
   render: h => h(App)
 })
+
+function setFen(fen){
+    document.getElementById('fen').value = fen
+    document.getElementById("clickbutton").click()
+}
+
+setTimeout(function(){                
+    setFen('rnb1kbnr/ppp1pppp/8/q7/8/2N5/PPPP1PPP/R1BQKBNR w KQkq - 2 4')
+}, 500)
